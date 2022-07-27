6c47b28233d97c1529faee751119418b5e3b43de
Fernando Joost <fjoostr@gmail.com>
2022-07-27 17:57:09 -0400

    inicializa composition api cuenta

--- hello-world/src/components/Cuenta.vue
+++ hello-world/src/components/Cuenta.vue
@@ -1,14 +1,22 @@
 <template>
-  <h3>Saldo: {{saldo}}</h3>
-  <h4>Cuenta: {{cuenta}}</h4>
-  <h4>Estado: {{estado ? 'Activa' : 'Cerrada'}}</h4>
-  <h4 class="mb-0">Servicios disponibles: </h4>
+  <h3>Saldo: {{ saldo }}</h3>
+  <h4>Cuenta: {{ cuenta }}</h4>
+  <h4>Estado: {{ estado ? "Activa" : "Cerrada" }}</h4>
+  <h4 class="mb-0">Servicios disponibles:</h4>
   <div v-for="(item, index) in servicios" :key="index">
-      {{ index + 1 }} - {{ item }}
+    {{ index + 1 }} - {{ item }}
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
+defineProps({
+    cuenta: 'Visa',
+    saldo: 5000,
+    estado: true,
+    servicios: ['pagos','giros','transferencias']
+})
+</script>
+<!-- <script>
 export default {
     data() {
         return {
@@ -19,10 +27,10 @@ export default {
         }
     }
 }
-</script>
+</script> -->
 
 <style>
-    .mb-0 {
-        margin-bottom: 0.5rem;
-    }
+.mb-0 {
+  margin-bottom: 0.5rem;
+}
 </style>
