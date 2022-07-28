e1a6b74f5ce4ae9a29b48d43d93b25b938f00663
Fernando Joost <fjoostr@gmail.com>
2022-07-28 07:44:09 -0400

    create project and contador HomeView

---
+++ composition-api/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>contador: {{ contador }}</h1>
+    <button @click="sumar">+</button>
+    <button @click="restar">-</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive, toRefs } from 'vue'
+
+export default {
+  setup () {
+    const state = reactive({
+      count: 0,
+    })
+  
+    return {
+      ...toRefs(state),
+    }
+  }
+}
+</script>
---
+++ composition-api/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1> contador: {{contador}}</h1>
+    <button @click="sumar">+</button>
+    <button @click="restar">-</button>
+  </div>
+</template>
+<!-- ejercicio, hacer un contador con un boton que (+) y (-) -->
+<script>
+import { defineComponent } from 'vue';
+export default {
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    sumar() {
+      this.contador ++
+      // return contador
+    },
+    restar() {
+      this.contador --
+      // return contador
+    }
+  },
+};
+</script>
