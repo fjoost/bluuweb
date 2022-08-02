f3dc790c71890af1296ad1f690b27cbd2a4f1a32
Fernando Joost <fjoostr@gmail.com>
2022-08-02 14:33:11 -0400

    ejemplo uso de propiedades computadas

--- compositionAPI/composition-api/src/views/AboutView.vue
+++ compositionAPI/composition-api/src/views/AboutView.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <h1>contador: {{ contador }}</h1>
-    <button @click="aumentar">aumentar</button>
-    <button @click="disminuir">disminuir</button>
+    <h1 :style="{ color: color }">contador: {{ contador }}</h1>
+    <button @click="aumentar">+</button>
+    <button @click="disminuir">-</button>
   </div>
 </template>
 
 <script>
+import { computed } from "@vue/reactivity";
 import { ref } from "vue";
 
 export default {
@@ -21,7 +22,6 @@ export default {
       /* contador es referencia, por lo tanto para acceder al valor se escribe */
       contador.value++;
     };
-
     const disminuir = () => {
       /* contador es referencia, por lo tanto para acceder al valor se escribe */
       contador.value--;
@@ -29,9 +29,18 @@ export default {
     /* todo lo que queramos utilizar en el template debe ser retornado dentro del objeto
       ej: quiero utilizar contador y la funcion de aumentar, que me entrega el result.
       por lo tanto se agrega al objeto
-    
+
     */
-    return { contador, aumentar, disminuir };
+
+    const color = computed(() => {
+      if (contador.value < 0) {
+        return "red";
+      } else {
+        return "blue";
+      }
+    });
+    /* computed recibe una función de flecha */
+    return { contador, aumentar, disminuir, color };
   },
 };
 </script>
